e2924cb4a40123e9096e24cf90190a16e453ab15
Onoulade <onoulade.geek@gmail.com>
2026-04-08 13:25:21 +0200

    Add Space Age compatibility scaffolding with chromatic printer baseline
    (cherry picked from commit 2a7003dd623928bad2c088e40d4e2e83cf7b94f3)

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -2,11 +2,15 @@
 
 local feature_flags = require("feature_flags")
 local working_hours_enabled = feature_flags.working_hours_enabled()
+local space_age_enabled = feature_flags.space_age_enabled()
 
 require("prototypes.recipe.paperwork")
 require("prototypes.recipe.resolution")
 require("prototypes.recipe.economy")
 require("prototypes.recipe.buildings")
+if space_age_enabled then
+  require("prototypes.recipe.space_age")
+end
 if working_hours_enabled then
   require("prototypes.recipe.modules")
 end
