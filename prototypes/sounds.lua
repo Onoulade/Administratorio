da22ceed1b6568fb0b17727ee27ebd4873d8cb42
Onoulade <onoulade.geek@gmail.com>
2026-04-28 16:01:58 +0200

    Better protest sounds

--- prototypes/sounds.lua
+++ prototypes/sounds.lua
@@ -4,14 +4,14 @@ data:extend({
     name = "administratorio-protest-alert",
     allow_random_repeat = true,
     variations = {
-      { filename = "__administratorio__/sound/alerts/protest-alert.ogg", volume = 0.65 },
-      { filename = "__administratorio__/sound/alerts/protest-crowd-outdoor.ogg", volume = 0.58 },
-      { filename = "__administratorio__/sound/alerts/protest-angry-crowd.ogg", volume = 0.62 },
+      { filename = "__administratorio__/sound/alerts/protest-1.ogg", volume = 0.4 },
+      { filename = "__administratorio__/sound/alerts/protest-2.ogg", volume = 0.4 },
+      { filename = "__administratorio__/sound/alerts/protest-3.ogg", volume = 0.4 },
     },
     aggregation = {
-      max_count = 2,
+      max_count = 8,
       remove = true,
-      count_already_playing = true,
+      count_already_playing = false,
     },
   },
 })
