d15a7fecff47bfaff6e0bd79103cb562abc1b25e
Onoulade <onoulade.geek@gmail.com>
2026-05-25 18:09:35 +0200

    feat(protests): add debug belt and inserter targets

--- feature_flags.lua
+++ feature_flags.lua
@@ -13,4 +13,8 @@ function M.working_hours_enabled()
   return startup_bool("administratorio-enable-working-hours", true)
 end
 
+function M.debug_protest_belts_and_inserters_enabled()
+  return startup_bool("administratorio-debug-protest-belts-and-inserters", false)
+end
+
 return M
